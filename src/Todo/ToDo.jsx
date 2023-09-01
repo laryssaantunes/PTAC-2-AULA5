f9fa60da51ef2628992c3d2cce1b6e7896d8d991
laryssaantunes <larissa.antunes.icloud@gmail.com>
2023-09-01 13:59:09 -0400

    commit alterei

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -1,13 +1,42 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+export default function todo () {
 
-export default function ToDo() {
-   
-    return (
-        <div>
-            <h1>Lista de Atividades</h1>
-            <Link to="/">home</Link>
-           
-        </div>
-    );
+  const [atividade, setAtividade] = useState("");
+  const [lista, setLista] = useState([]);
+  const [id,setId] = useState(1);
+    const salvar =(e) =>{
+        e.preventDefault();
+        setLista([...lista, {
+                atividade: atividade,
+                id: id
+        }]);
+        setId(id + 1);
+        setAtividade("");
+    };
+    const remover = (id) => {
+      setLista(lista.filter((ativ) => (ativ.id !== id ? lista : null)))
+      const auxLista = [];
+      lista.map((lista) => {
+        if(lista.id !== id){
+          auxLista.push(lista);
+        }
+      });
+      setLista(auxLista);
+    }
+
+  return (
+    <div>
+      <Link to="/">home</Link>
+      <h1>Lista de Atividades</h1>
+      <form onSubmit={salvar}>
+        <input value={atividade} type="text"
+             onChange={(e) => setAtividade(e.target.value)} />
+
+        <button>ADD</button>
+      </form>
+      {lista.map((ativ)=> <p>{ativ.atividade}</p>)}
+
+    </div>
+  ); 
 }
