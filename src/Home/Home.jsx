b627c8a157f464ae7fa0b5d33c8437dd4d25c172
laryssaantunes <larissa.antunes.icloud@gmail.com>
2023-09-20 10:49:18 -0400

    commit estou fazendo o home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./home.css"
 export default function Home() {
    
     return (
-        <div>
-            <h1>Home</h1>
-            <Link to="/todo">todo</Link>
+        <div className= "navbar">
+            <img src="/https://w7.pngwing.com/pngs/12/377/png-transparent-logo-brand-font-makeup-computer-logo-computer-wallpaper.png.png" alt="logo" className= "logo"/>
+            <Link to="/todo" className="button-link"> Entra / Cadastrar-se</Link>
         </div>
+
 
     );
 }
